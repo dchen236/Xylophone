d00fd3a3f07368ab4913d8ee88b8993f72bfd7b3
Danni <dannichen236@gmail.com>
2018-12-19 21:24:06 -0800

    playing the selected node

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,8 +20,13 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
+        let tag = sender.tag
+        let pathUrl = Bundle.main.url(forResource: "note\(tag)", withExtension: "wav")
+        do {
+            audioPlayer =  try AVAudioPlayer(contentsOf: pathUrl!)
+        } catch let error {
+            print(error.localizedDescription)
+        }
         
     }
     
